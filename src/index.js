6a43dea5da8d687f1dc0521ebc2a43210da5bccb
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-06-27 06:44:53 -0400

    added basic files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import './style.css';
+import Icon from './images/icon.jpg';
+
+
+const main = document.createElement('div');
+const icon = new Image();
+icon.src = Icon;
+main.innerText = "Hello";
+main.appendChild(icon);
+
 console.log("Hello");
+
+document.body.appendChild(main);
